b8d42ea0858dbe363cb469e7530e3069d4c4d9c5
mrpyrex <ndifrkeumoren@gmail.com>
2021-05-03 11:03:57 +0100

    Added about us page

--- src/components/pages/Aboutpage.jsx
+++ src/components/pages/Aboutpage.jsx
@@ -4,6 +4,60 @@ const Aboutpage = () => {
   return (
     <div className="container">
       <h2 className="text-center my-4">About Us</h2>
+      <div className="col-md-8">
+        <div className="row">
+          <h2>About Us</h2>
+          <p>
+            Brixxfauce Limited is a dynamic and efficient architectural and
+            building construction firm that has been providing complete
+            architectural design, building construction, master planning,
+            landscapping and interior design services since 2010. Our clients
+            include major local and international developers, private investors,
+            retailers and big nationl coparations and government organizations.
+            We are organized into several open studios around different types of
+            projects. Our portfolio spans residential, commecial and office
+            projects. Our experience gives us the knowledge base for many
+            projects types and a clear understanding of client expectations. We
+            are a team of highly qualified professional that believe in unique,
+            but sound and functional design.
+          </p>
+          <h2>Our Mission</h2>
+          <p>
+            We provide quality building construction and architectural services
+            that result in creative, functional designs which have a lasting
+            impact on their surrounding environment. Our success in achieved
+            through dedication and team work as we develop relationship with our
+            clients and communities to understand and meet their unique needs.
+            Brixxforce mission is to achieve construction. Excellence through a
+            thoughtful building process which provides each project with an
+            overriding concept. We pursue this goal through a careful project.
+            We encourage the client to be actively involved throughout the
+            design and construction process and to participate in molding the
+            appropriate construction solution. Through this process, we believe
+            both the client and the team have the duty to contribute to the
+            creation process
+          </p>
+          <h2>Our Philosophy</h2>
+          <p>
+            Our approach is to start each project by identifying with clients
+            input, the principles, values and drivers of the project. We take
+            pride in delivering work which is sensitive to the environment as
+            well as to the context and scales of each site and that is also
+            reflective to the client unique taste and lifestyle,
+          </p>
+          <h2>Our Services</h2>
+          <p>
+            Brixxforce Groups is experienced in all phases of building
+            construction practice from site analysis and feasibility studies,
+            through construction surveillance our experience spam a wide range
+            of building types and construction practices with an emphasis on
+            high quality custom homes. Our interests extend to the design of
+            lighting, hardware and furniture as an integral part of
+            architectural design. We create spaces that enhance the human
+            condition and allow for the evolution of the spirit.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
