f230d6389df9bd739181353feca8b9890a654833
mrpyrex <ndifrkeumoren@gmail.com>
2021-07-12 17:40:18 +0100

    Added extra pics fields to projects

--- src/components/pages/admin/AddProject.jsx
+++ src/components/pages/admin/AddProject.jsx
@@ -6,10 +6,22 @@ const AddProject = () => {
   const [description, setDescription] = useState("");
   const [date, setDate] = useState("");
   const [mainpic, setMainpic] = useState(null);
+  const [picOne, setPicOne] = useState(null);
+  const [picTwo, setPicTwo] = useState(null);
 
   const handleChange = (e) => {
     const file = e.target.files[0];
     setMainpic(file);
+  };
+
+  const handlePicOneChange = (e) => {
+    const file = e.target.files[0];
+    setPicOne(file);
+  };
+
+  const handlePicTwoChange = (e) => {
+    const file = e.target.files[0];
+    setPicTwo(file);
   };
 
   const handleSubmit = (e) => {
@@ -19,6 +31,8 @@ const AddProject = () => {
     data.append("title", title);
     data.append("description", description);
     data.append("main_pic", mainpic);
+    data.append("pic_one", picOne);
+    data.append("pic_two", picTwo);
     data.append("date", date);
 
     try {
@@ -43,6 +57,7 @@ const AddProject = () => {
             Project Title
           </label>
           <input
+            required
             type="text"
             className="form-control"
             id="title"
@@ -55,6 +70,7 @@ const AddProject = () => {
             Project Description
           </label>
           <textarea
+            required
             className="form-control"
             id="description"
             rows="3"
@@ -66,6 +82,7 @@ const AddProject = () => {
             Select Project Main Picture
           </label>
           <input
+            required
             accept="image/*"
             className="form-control"
             type="file"
@@ -74,7 +91,35 @@ const AddProject = () => {
           />
         </div>
         <div className="mb-3">
+          <label htmlFor="picone" className="form-label">
+            Select Project Other Picture
+          </label>
           <input
+            accept="image/*"
+            className="form-control"
+            type="file"
+            id="picone"
+            onChange={handlePicOneChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="pictwo" className="form-label">
+            Select Project Other Picture
+          </label>
+          <input
+            accept="image/*"
+            className="form-control"
+            type="file"
+            id="pictwo"
+            onChange={handlePicTwoChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="date" className="form-label">
+            Select Project Date
+          </label>
+          <input
+            className="form-control"
             type="date"
             id="date"
             onChange={(e) => setDate(e.target.value)}
